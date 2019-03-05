5434f21529bb26456e40c5af1cb5bbfeed8d65cd
David Witter <david.witter@wisc.edu>
2019-03-05 15:38:00 -0600

    add top-bar component and rollup

---
+++ rollup.config.js
@@ -0,0 +1,76 @@
+import { rollup } from 'rollup';
+import html from 'rollup-plugin-html';
+import minify from 'rollup-plugin-minify-es';
+
+let fileName = 'top-bar';
+let directory = 'top-bar';
+let objName = 'UWDSTopBar';
+
+
+let plugins = {
+  full: [
+    html({
+      include: `components/${directory}/${fileName}.html`,
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true
+      }
+    })
+  ],
+  min: [
+    html({
+      include: `components/${directory}/${fileName}.html`,
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true
+      }
+    }),
+    minify({
+      output: {
+        wrap_iife: true
+      }
+    })
+  ]
+};
+
+
+export default [
+  {
+    input: `components/${directory}/${fileName}.js`,
+    plugins: plugins.full,
+    output: {
+      file: `dist/${fileName}.js`,
+      name: objName,
+      format: 'iife'
+    }
+  },
+  {
+    input: `components/${directory}/${fileName}.js`,
+    plugins: plugins.min,
+    output: {
+      file: `dist/${fileName}.min.js`,
+      name: objName,
+      format: 'iife'
+    }
+  },
+  {
+    input: `components/${directory}/${fileName}.js`,
+    plugins: plugins.full,
+    output: {
+      file: `dist/${fileName}.mjs`,
+      name: objName,
+      format: 'es'
+    }
+  },
+  {
+    input: `components/${directory}/${fileName}.js`,
+    plugins: plugins.min,
+    output: {
+      file: `dist/${fileName}.min.mjs`,
+      name: objName,
+      format: 'es'
+    }
+  },
+];
